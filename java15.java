69698de76bb5c062b69aa087dd5ff992e099e9a1
Aarti chavda <aarti22rajput@gmail.com>
2022-07-19 22:52:45 +0100

    my first programme

---
+++ java15.java
@@ -0,0 +1,54 @@
+package jv15;
+
+public class java15 {
+
+    public static void main(String[] args) {
+        // Integer //
+        byte a = 80;
+        byte b = 30;
+        System.out.println("data type byte");
+        System.out.println(" addition = " + (a + b));
+        System.out.println("subtraction =" + (a - b));
+        System.out.println("multiplication =" + (a * b));
+        System.out.println("division =" + (a / b));
+        short c = 3276;
+        short d = 3277;
+        System.out.println("data type short");
+        System.out.println("addition =" + (c + d));
+        System.out.println("subtraction =" + (c - d));
+        System.out.println("multiplication =" + (c * d));
+        System.out.println("division =" + (c / d));
+        long e = 100000l;
+        long f = 100001l;
+        System.out.println("data type long");
+        System.out.println("addition = " + (e + f));
+        System.out.println("multiplication =" + (e * f));
+        System.out.println("subtraction =" + (e - f));
+        System.out.println("division =" + (e / f));
+        int g = 1182;
+        int h = 2211;
+        System.out.println("data type int");
+        System.out.println("addition =" + (g + h));
+        System.out.println("multiplication = " + (g * h));
+        System.out.println("subtraction =" + (g - h));
+        System.out.println("division =" + (g / h));
+        float i = 22.11f;
+        float j = 11.22f;
+        System.out.println("data type float");
+        System.out.println("addition =" + (i + j));
+        System.out.println("multiplication =" + (i * j));
+        System.out.println("subtraction =" + (i - j));
+        System.out.println("division=" + (i / j));
+        double k = 899.3d;
+        double l = 456.4d;
+        System.out.println("data type double");
+        System.out.println("addition =" + (k + l));
+        System.out.println("multiplication =" + (k * l));
+        System.out.println("division =" + (k / l));
+        System.out.println("subtraction =" + (k - l));
+        Character m = 'A';
+        System.out.println("data type character");
+        System.out.println(m);
+
+
+    }
